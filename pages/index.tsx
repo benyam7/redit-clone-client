c9f7c46e0f27219907b277abfac48d975b1bdb35
Benyam Seifu <45682486+benyam7@users.noreply.github.com>
2021-02-14 10:56:14 +0300

    feat: top subs

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,9 +4,11 @@ import { useState, useEffect, Fragment } from "react";
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
 
-import { Post } from "../types";
+import { Post, Sub } from "../types";
 import PostCard from "../componets/PostCard";
 import useSWR from "swr";
+import Image from "next/image";
+import Link from "next/link";
 
 // import { GetServerSideProps } from "next";
 
@@ -21,6 +23,7 @@ export default function Home(/* { posts } */) {
   // }, []);
 
   const { data: posts } = useSWR("/posts");
+  const { data: topSubs } = useSWR("/misc/top-subs");
 
   return (
     <Fragment>
@@ -37,6 +40,39 @@ export default function Home(/* { posts } */) {
           ))}
         </div>
         {/* sidebar */}
+        <div className="ml-6 w-80">
+          <div className="bg-white rounded">
+            <div className="p-4 border-b-2">
+              <p className="text-lg font-semibold text-center">
+                Top Communities
+              </p>
+            </div>
+            <div>
+              {topSubs?.map((sub: Sub) => {
+                return (
+                  <div className="flex items-center px-4 py-2 text-xs border-b">
+                    <div className="mr-2 overflow-hidden rounded-full cursor-pointer">
+                      <Link href={`/r/${sub.name}`}>
+                        <Image
+                          src={sub.imageUrl}
+                          alt="Sub"
+                          width={(6 * 16) / 4}
+                          height={(6 * 16) / 4}
+                        />
+                      </Link>
+                    </div>
+                    <Link href={`/r/${sub.name}`}>
+                      <a className="font-bold hover:cursor-pointer">
+                        /r/{sub.name}
+                      </a>
+                    </Link>
+                    <p className="ml-auto font-medium">{sub.postCount}</p>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        </div>
       </div>
     </Fragment>
   );
